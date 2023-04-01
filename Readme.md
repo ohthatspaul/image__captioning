50380fa0dcc2686b6c8fcb206f07b069b2c8433c
Talent Mavingire <mavingiretalent@gmail.com>
2023-04-01 08:36:26 +0200

    clean up and test image

--- Readme.md
+++ Readme.md
@@ -1,27 +1,37 @@
 # Image Captioning
 
-# Dataset
+## Dataset
 
-> ### Images : [Flickr8k_Dataset](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip)
->
-> ### Text : [Flickr8k_text](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip)
->
-> <br>
+---
 
-# Data preprocessing and feature extraction
+## Images : [Flickr8k_Dataset](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip)
 
-> <br>
+## Text : [Flickr8k_text](https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip)
 
-# Built and trained Encoder-Decoder Model
+## Data preprocessing and feature extraction
 
-> <br>
+---
 
-# Model Evaluation
+When using the RNN as the languate model and a affine network to generate words, we need to feed the already generated caption into the model and get the next word. Therefore, to generate a caption of n words, the mode needs to run n+1 times (n words plus token). During training, we also need to run the model n+1 times, and generate a separate training sequence for each run. There are 6000 images in the training data set, and 5 captions for each image. The maximum length of the caption is 33 words. This comes to a maximum of <code> 6000×5×33 or 990,000 </code> training samples. The training data is generated on-the-fly, just before the model requires it. That is, we will generate the training data one batch at a time, and then input the data into the model as needed (Progressive Loading).
 
-> The features of the images to be captioned are extracted using a VGG16 CNN that as been pretrained using ImageNet data. The output of the FC8 layer of VGG16 is fed to another dense layer to extract the image feature. The weights of the VGG16 CNN are frozen when we train our RNN language generator. The additional dense layer is trainable
-> The words are embedded using the pretrained wording GloVE embeddings. The embeddings are not trained further in our generative model.
-> The image features and word embeddings are concatenated together and input to an LSTM RNN. Te output of the RNN is sampled as used as caption to the image. A special "START" token is used to initialize the generation process, and the process is terminated when an "END" token is sampled.
+We first define a module that a caption of <code>length n and generates the n+1 </code>training data.
 
-> <br>
+## Building and trainining Encoder-Decoder Model
 
-# Hosting and Deployment
+---
+
+Made use of the VGG16 model as the base model for the CNN. We replace the last softmax layer freeze with another affine layer with 256 output and add a dropout layer. The original layers of the VGG16 model is frozen. The image is input into the input of the VGG16 layer. The GLOVE embedding parameters are also frozen. Words are fed as input to the embedding. The output of the embedding is fed into an LSTM RNN with 256 states. The output of the LSTM (256 dimensionss) and the output of the CNN (256 dimensions) is concatenated together to for a 512 dimensional input to a dense layer. The output of the dense layer is fed into a softmax function.
+
+> [model_concat image]
+
+At each time step of the RNN the a word in the training caption will be fed into the input of the RNN. The out of the RNN will be compared with the ground truth next word. The training captions were tokenized and embedded using the GLOVE word embeddings. The embeddings were fed into the RNN.
+
+ <br>
+
+## Model Evaluation
+
+---
+
+The model outputs a one-hot vector and the error was measured using categorical crossentropy.Generating novel image captions using the trained model. Test images and images from the internet were used as input to the trained model to generate captions. The captions were examined to determine the weaknesses of the model and suggest improvements. Beam search was used to generate better captions using the model.The model was evaluated using the BLEU and ROUGE metric. Used a RNN as a language model. With this approach, the word embeddings are input to the RNN, and the final state of the RNN is combined with image features and input to another neural network to predict the next word in the caption. The features of the images to be captioned are extracted using a VGG16 CNN that as been pretrained using ImageNet data. The output of the <code> FC8 layer of VGG16 </code> is used as the image feature. The weights of the VGG16 CNN are frozen when we train our RNN language generator. The additional dense layer is trainable. The words are embedded using the pretrained wording GloVE embeddings. The embeddings are not trained further in our generative model. The image features and word embeddings are concatenated together and input to an LSTM RNN. The output of the RNN is sampled as used as caption to the image. A special "START" token is used to initialize the generation process, and the process is terminated when an "END" token is sampled.
+
+## Hosting and Deployment
--- image_captioning.py
+++ image_captioning.py
@@ -87,23 +87,23 @@ features = dict()
 
 # #  We first define a function that can load the training/test/dev ids that are stored in corresponding files.
 
-def load_data_set_ids(filename):
-    file =  open(filename, 'r')
-    text = file.read()
-    file.close()
+# def load_data_set_ids(filename):
+#     file =  open(filename, 'r')
+#     text = file.read()
+#     file.close()
     
-    dataset = list()
-    for image_id in text.split('\n'):
-        if len(image_id) < 1:
-            continue
+#     dataset = list()
+#     for image_id in text.split('\n'):
+#         if len(image_id) < 1:
+#             continue
             
-        dataset.append(image_id)
+#         dataset.append(image_id)
     
-    return set(dataset)
+#     return set(dataset)
 
-training_set = load_data_set_ids('Flickr8k_text/Flickr_8k.trainImages.txt')
-dev_set = load_data_set_ids('Flickr8k_text/Flickr_8k.devImages.txt')
-test_set = load_data_set_ids('Flickr8k_text/Flickr_8k.testImages.txt')
+# training_set = load_data_set_ids('Flickr8k_text/Flickr_8k.trainImages.txt')
+# dev_set = load_data_set_ids('Flickr8k_text/Flickr_8k.devImages.txt')
+# test_set = load_data_set_ids('Flickr8k_text/Flickr_8k.testImages.txt')
 
 # After the images for each set is identified, we clean up the captions by:
 # - Removing all numbers and punctuations
@@ -112,187 +112,187 @@ test_set = load_data_set_ids('Flickr8k_text/Flickr_8k.testImages.txt')
 # - Add 'START' and 'END' to the target data
 
 
-filename = 'Flickr8k_text/Flickr8k.token.txt'
-file =  open(filename, 'r')
-token_text = file.read()
-file.close()
+# filename = 'Flickr8k_text/Flickr8k.token.txt'
+# file =  open(filename, 'r')
+# token_text = file.read()
+# file.close()
 
-translator = str.maketrans("", "", string.punctuation) #translation table that maps all punctuation to None
-image_captions = dict()
-image_captions_train = dict()
-image_captions_dev = dict()
-image_captions_test = dict()
-image_captions_other = dict()
-corpus = list() #corpus used to train tokenizer
-corpus.extend(['<START>', '<END>', '<UNK>']) #add SOS and EOS to list first
+# translator = str.maketrans("", "", string.punctuation) #translation table that maps all punctuation to None
+# image_captions = dict()
+# image_captions_train = dict()
+# image_captions_dev = dict()
+# image_captions_test = dict()
+# image_captions_other = dict()
+# corpus = list() #corpus used to train tokenizer
+# corpus.extend(['<START>', '<END>', '<UNK>']) #add SOS and EOS to list first
 
-max_imageCap_len = 0
+# max_imageCap_len = 0
 
-for line in token_text.split('\n'): # first split on new line
-    tokens = line.split(' ') #split on white space, the first segment is 1000268201_693b08cb0e.jpg#0, the following segements are caption texts
-    if len(line) < 2:
-        continue
-    image_id, image_cap = tokens[0], tokens[1:] #use the first segment as image id, the rest as caption
-    image_id = image_id.split('#')[0] #strip out #0 from filename
-    image_cap = ' '.join(image_cap) #join image caption together again
+# for line in token_text.split('\n'): # first split on new line
+#     tokens = line.split(' ') #split on white space, the first segment is 1000268201_693b08cb0e.jpg#0, the following segements are caption texts
+#     if len(line) < 2:
+#         continue
+#     image_id, image_cap = tokens[0], tokens[1:] #use the first segment as image id, the rest as caption
+#     image_id = image_id.split('#')[0] #strip out #0 from filename
+#     image_cap = ' '.join(image_cap) #join image caption together again
 
-    image_cap = image_cap.lower() #change to lower case
-    image_cap = image_cap.translate(translator) #take out punctuation using a translation table
+#     image_cap = image_cap.lower() #change to lower case
+#     image_cap = image_cap.translate(translator) #take out punctuation using a translation table
     
-    image_cap = image_cap.split(' ') #split string here because following two methods works on word-level best
-    image_cap = [w for w in image_cap if w.isalpha()] #keep only words that are all letters
-    image_cap = [w for w in image_cap if len(w)>1]
-    image_cap = '<START> ' + ' '.join(image_cap) + ' <END>' #add sentence start/end; note syntax: separator.join()
+#     image_cap = image_cap.split(' ') #split string here because following two methods works on word-level best
+#     image_cap = [w for w in image_cap if w.isalpha()] #keep only words that are all letters
+#     image_cap = [w for w in image_cap if len(w)>1]
+#     image_cap = '<START> ' + ' '.join(image_cap) + ' <END>' #add sentence start/end; note syntax: separator.join()
     
-    #update maximum caption length
-    if len(image_cap.split()) > max_imageCap_len:
-        max_imageCap_len = len(image_cap.split())
+#     #update maximum caption length
+#     if len(image_cap.split()) > max_imageCap_len:
+#         max_imageCap_len = len(image_cap.split())
     
-    #add to dictionary
-    if image_id not in image_captions:
-        image_captions[image_id] = list() #create a new list if it does not yet exist
-    image_captions[image_id].append(image_cap)
+#     #add to dictionary
+#     if image_id not in image_captions:
+#         image_captions[image_id] = list() #create a new list if it does not yet exist
+#     image_captions[image_id].append(image_cap)
     
-    #add to train/dev/test dictionaries
-    if image_id in training_set:
-        if image_id not in image_captions_train:
-            image_captions_train[image_id] = list() #creat a new list if it does not yet exist
-        image_captions_train[image_id].append(image_cap)
-        corpus.extend(image_cap.split()) #add only training words to corpus to train tokenlizer
+#     #add to train/dev/test dictionaries
+#     if image_id in training_set:
+#         if image_id not in image_captions_train:
+#             image_captions_train[image_id] = list() #creat a new list if it does not yet exist
+#         image_captions_train[image_id].append(image_cap)
+#         corpus.extend(image_cap.split()) #add only training words to corpus to train tokenlizer
         
-    elif image_id in dev_set:
-        if image_id not in image_captions_dev:
-            image_captions_dev[image_id] = list() #creat a new list if it does not yet exist
-        image_captions_dev[image_id].append(image_cap)
+#     elif image_id in dev_set:
+#         if image_id not in image_captions_dev:
+#             image_captions_dev[image_id] = list() #creat a new list if it does not yet exist
+#         image_captions_dev[image_id].append(image_cap)
         
-    elif image_id in test_set:
-        if image_id not in image_captions_test:
-            image_captions_test[image_id] = list() #creat a new list if it does not yet exist
-        image_captions_test[image_id].append(image_cap)
-    else:
-        if image_id not in image_captions_other:
-            image_captions_other[image_id] = list() #creat a new list if it does not yet exist
-        image_captions_other[image_id].append(image_cap)
+#     elif image_id in test_set:
+#         if image_id not in image_captions_test:
+#             image_captions_test[image_id] = list() #creat a new list if it does not yet exist
+#         image_captions_test[image_id].append(image_cap)
+#     else:
+#         if image_id not in image_captions_other:
+#             image_captions_other[image_id] = list() #creat a new list if it does not yet exist
+#         image_captions_other[image_id].append(image_cap)
 
-caption_train_tokenizer = Tokenizer() #initialize tokenizer
-caption_train_tokenizer.fit_on_texts(corpus) #fit tokenizer on training data
+# caption_train_tokenizer = Tokenizer() #initialize tokenizer
+# caption_train_tokenizer.fit_on_texts(corpus) #fit tokenizer on training data
     
-fid = open("image_captions.pkl","wb")
-dump(image_captions, fid)
-fid.close()
+# fid = open("image_captions.pkl","wb")
+# dump(image_captions, fid)
+# fid.close()
 
-fid = open("image_captions_train.pkl","wb")
-dump(image_captions_train, fid)
-fid.close()
+# fid = open("image_captions_train.pkl","wb")
+# dump(image_captions_train, fid)
+# fid.close()
 
 
-fid = open("image_captions_dev.pkl","wb")
-dump(image_captions_dev, fid)
-fid.close()
+# fid = open("image_captions_dev.pkl","wb")
+# dump(image_captions_dev, fid)
+# fid.close()
 
-fid = open("image_captions_test.pkl","wb")
-dump(image_captions_test, fid)
-fid.close()
+# fid = open("image_captions_test.pkl","wb")
+# dump(image_captions_test, fid)
+# fid.close()
 
-fid = open("image_captions_other.pkl","wb")
-dump(image_captions_other, fid)
-fid.close()
+# fid = open("image_captions_other.pkl","wb")
+# dump(image_captions_other, fid)
+# fid.close()
 
-fid = open("caption_train_tokenizer.pkl","wb")
-dump(caption_train_tokenizer, fid)
-fid.close()
+# fid = open("caption_train_tokenizer.pkl","wb")
+# dump(caption_train_tokenizer, fid)
+# fid.close()
 
-fid = open("corpus.pkl","wb")
-dump(corpus, fid)
-fid.close()
+# fid = open("corpus.pkl","wb")
+# dump(corpus, fid)
+# fid.close()
 
-corpus_count=Counter(corpus)
-fid = open("corpus_count.pkl","wb")
-dump(corpus_count, fid)
-fid.close()
+# corpus_count=Counter(corpus)
+# fid = open("corpus_count.pkl","wb")
+# dump(corpus_count, fid)
+# fid.close()
 
-print("size of data =", len(image_captions), "size of training data =", len(image_captions_train), "size of dev data =", len(image_captions_dev), "size of test data =", len(image_captions_test), "size of unused data =", len(image_captions_other))
-print("maximum image caption length =",max_imageCap_len)
+# print("size of data =", len(image_captions), "size of training data =", len(image_captions_train), "size of dev data =", len(image_captions_dev), "size of test data =", len(image_captions_test), "size of unused data =", len(image_captions_other))
+# print("maximum image caption length =",max_imageCap_len)
 
-embeddings_index = dict()
-fid = open('glove.6B/glove.6B.50d.txt' ,encoding="utf8") 
-for line in fid:
-    values = line.split()
-    word = values[0]
-    coefs = np.asarray(values[1:], dtype='float32')
-    embeddings_index[word] = coefs
-fid.close()
-EMBEDDING_DIM = 50
-word_index = caption_train_tokenizer.word_index
-embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+# embeddings_index = dict()
+# fid = open('glove.6B/glove.6B.50d.txt' ,encoding="utf8") 
+# for line in fid:
+#     values = line.split()
+#     word = values[0]
+#     coefs = np.asarray(values[1:], dtype='float32')
+#     embeddings_index[word] = coefs
+# fid.close()
+# EMBEDDING_DIM = 50
+# word_index = caption_train_tokenizer.word_index
+# embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
 
-for word, idx in word_index.items():
-    embed_vector = embeddings_index.get(word)
-    if embed_vector is not None:
-        # words not found in embedding index will be all-zeros.
-        embedding_matrix[idx] = embed_vector
+# for word, idx in word_index.items():
+#     embed_vector = embeddings_index.get(word)
+#     if embed_vector is not None:
+#         # words not found in embedding index will be all-zeros.
+#         embedding_matrix[idx] = embed_vector
         
-fid = open("embedding_matrix.pkl","wb")
-dump(embedding_matrix, fid)
-fid.close()
+# fid = open("embedding_matrix.pkl","wb")
+# dump(embedding_matrix, fid)
+# fid.close()
 
-def create_sequences(tokenizer, max_length, desc_list, photo, vocab_size):
-    X1, X2, y = list(), list(), list()
-    # walk through each description for the image
-    for desc in desc_list:
-        # encode the sequence
-        seq = tokenizer.texts_to_sequences([desc])[0] #[0] is used to take out the extra dim. This changes from text to a number
-        # split one sequence into multiple X,y pairs
-        for i in range(1, len(seq)):
-            # split into input and output pair
-            in_seq, out_seq = seq[:i], seq[i]
-            # pad input sequence
-            in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
-            # encode output sequence
-            # import pdb; pdb.set_trace()
-            out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
-            # store
-            X1.append(photo)
-            X2.append(in_seq)
-            y.append(out_seq)
-    return np.array(np.squeeze(X1)), np.array(X2), np.array(y)
+# def create_sequences(tokenizer, max_length, desc_list, photo, vocab_size):
+#     X1, X2, y = list(), list(), list()
+#     # walk through each description for the image
+#     for desc in desc_list:
+#         # encode the sequence
+#         seq = tokenizer.texts_to_sequences([desc])[0] #[0] is used to take out the extra dim. This changes from text to a number
+#         # split one sequence into multiple X,y pairs
+#         for i in range(1, len(seq)):
+#             # split into input and output pair
+#             in_seq, out_seq = seq[:i], seq[i]
+#             # pad input sequence
+#             in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+#             # encode output sequence
+#             # import pdb; pdb.set_trace()
+#             out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+#             # store
+#             X1.append(photo)
+#             X2.append(in_seq)
+#             y.append(out_seq)
+#     return np.array(np.squeeze(X1)), np.array(X2), np.array(y)
 
-# data generator, intended to be used in a call to model.fit_generator()
-def data_generator(descriptions, photos, tokenizer, max_length, batch_size, vocab_size):
-    # loop for ever over images
-    current_batch_size=0
-    while 1:
-        for key, desc_list in descriptions.items():
-            # retrieve the photo feature
-            if current_batch_size == 0:
-                X1, X2, Y = list(), list(), list()
+# # data generator, intended to be used in a call to model.fit_generator()
+# def data_generator(descriptions, photos, tokenizer, max_length, batch_size, vocab_size):
+#     # loop for ever over images
+#     current_batch_size=0
+#     while 1:
+#         for key, desc_list in descriptions.items():
+#             # retrieve the photo feature
+#             if current_batch_size == 0:
+#                 X1, X2, Y = list(), list(), list()
             
-            imageFeature_id = key.split('.')[0]
-            photo = photos[imageFeature_id][0]
-            in_img, in_seq, out_word = create_sequences(tokenizer, max_length, desc_list, photo, vocab_size)
-            #in_img = np.squeeze(in_img)
-            X1.extend(in_img)
-            X2.extend(in_seq)
-            Y.extend(out_word)
-            current_batch_size += 1
-            if current_batch_size == batch_size:
-                current_batch_size = 0
-                yield [[np.array(X1), np.array(X2)], np.array(Y)]
+#             imageFeature_id = key.split('.')[0]
+#             photo = photos[imageFeature_id][0]
+#             in_img, in_seq, out_word = create_sequences(tokenizer, max_length, desc_list, photo, vocab_size)
+#             #in_img = np.squeeze(in_img)
+#             X1.extend(in_img)
+#             X2.extend(in_seq)
+#             Y.extend(out_word)
+#             current_batch_size += 1
+#             if current_batch_size == batch_size:
+#                 current_batch_size = 0
+#                 yield [[np.array(X1), np.array(X2)], np.array(Y)]
 
 
-fid = open('features.pkl', 'rb')
-image_features = load(fid)
-fid.close()
+# fid = open('features.pkl', 'rb')
+# image_features = load(fid)
+# fid.close()
 
-# test the data generator
-caption_max_length = 33
-batch_size = 1
-vocab_size = 7057
-generator = data_generator(image_captions_train, image_features, caption_train_tokenizer, caption_max_length, batch_size, vocab_size)
-inputs, outputs = next(generator)
-print(inputs[0].shape)
-print(inputs[1].shape)
-print(outputs.shape)
+# # test the data generator
+# caption_max_length = 33
+# batch_size = 1
+# vocab_size = 7057
+# generator = data_generator(image_captions_train, image_features, caption_train_tokenizer, caption_max_length, batch_size, vocab_size)
+# inputs, outputs = next(generator)
+# print(inputs[0].shape)
+# print(inputs[1].shape)
+# print(outputs.shape)
 
 def define_model_concat(vocab_size, max_length, embedding_matrix):
     # feature extractor model
@@ -352,23 +352,23 @@ vocab_size = 7506
 # steps = len(image_captions_train)
 # steps_per_epoch = np.floor(steps/batch_size)
 
-batch_size = 6
-steps = len(image_captions_train)
-steps_per_epoch = np.floor(steps/batch_size)
+# batch_size = 6
+# steps = len(image_captions_train)
+# steps_per_epoch = np.floor(steps/batch_size)
 
-epochs = 3
+# epochs = 3
 
-for i in range(epochs):
-	# create the data generator
-	generator = data_generator(image_captions_train, image_features, caption_train_tokenizer, caption_max_length, batch_size, vocab_size)
-	# fit for one epoch
-	post_rnn_model_concat_hist=post_rnn_model_concat.fit_generator(generator, epochs=1, steps_per_epoch=steps, verbose=1)
-	# save model
-	post_rnn_model_concat.save('modelConcat_1_' + str(i) + '.h5')
+# for i in range(epochs):
+# 	# create the data generator
+# 	generator = data_generator(image_captions_train, image_features, caption_train_tokenizer, caption_max_length, batch_size, vocab_size)
+# 	# fit for one epoch
+# 	post_rnn_model_concat_hist=post_rnn_model_concat.fit_generator(generator, epochs=1, steps_per_epoch=steps, verbose=1)
+# 	# save model
+# 	post_rnn_model_concat.save('modelConcat_1_' + str(i) + '.h5')
         
-# After importing the packages, we will load the VGG16 network. This neural network is the same network used to extract the features in our image captioning model. 
-# Loading it will take several minutes, so we will load it only once here. We will also load the tokenizer saved before when we were pre-processing the training data. 
-# We will also define a function to extract the image features
+# # After importing the packages, we will load the VGG16 network. This neural network is the same network used to extract the features in our image captioning model. 
+# # Loading it will take several minutes, so we will load it only once here. We will also load the tokenizer saved before when we were pre-processing the training data. 
+# # We will also define a function to extract the image features
 
 base_model = VGG16(include_top=True)
 feature_extract_pred_model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc2').output)
@@ -388,7 +388,7 @@ caption_train_tokenizer = load(open('caption_train_tokenizer.pkl', 'rb'))
 max_length = 33
 # load the model
 #pred_model = load_model('model_3_0.h5')
-pred_model = load_model('modelConcat_1a_2.h5') 
+pred_model = load_model('modelConcat_1_2.h5') 
 
 def generate_caption(pred_model, caption_train_tokenizer, photo, max_length):
     in_text = '<START>'
@@ -512,7 +512,7 @@ fid.close()
 # pre-define the max sequence length (from training)
 max_length = 33
 # load the model
-pred_model = load_model('modelConcat_1a_2.h5')
+pred_model = load_model('modelConcat_1_2.h5')
 #pred_model = load_model('model_3_0.h5')
 base_model = VGG16(include_top=True) #define the image feature extraction model
 feature_extract_pred_model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc2').output)
